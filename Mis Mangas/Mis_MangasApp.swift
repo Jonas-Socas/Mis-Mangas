a01f725fb5a8df8b9b16deddbf7eab224ed772f9
Jonás Socas <jonas@socas.dev>
2024-09-05 10:13:40 +0100

    Initial Commit

---
+++ Mis Mangas/Mis_MangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mis_MangasApp.swift
+//  Mis Mangas
+//
+//  Created by Jonás Socas on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Mis_MangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
